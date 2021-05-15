93e2375498331faa39700a61a6cb7079c9dbfec6
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-05-15 11:44:47 +0530

    Revert "sm8250-common: power-libperfmgr: Scan input devices for eligible DT2W node"
    This reverts commit 3dfa9762d508a1c13909bc15873c518d1fb0f222.
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- power-libperfmgr/hidl/Power.cpp
+++ power-libperfmgr/hidl/Power.cpp
@@ -30,9 +30,6 @@
 
 #include "AudioStreaming.h"
 #include "Power.h"
-#include <linux/input.h>
-constexpr int kWakeupModeOff = 4;
-constexpr int kWakeupModeOn = 5;
 
 namespace android {
 namespace hardware {
@@ -207,57 +204,8 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
     return Void();
 }
 
-int open_ts_input() {
-    int fd = -1;
-    DIR *dir = opendir("/dev/input");
-
-    if (dir != NULL) {
-        struct dirent *ent;
-
-        while ((ent = readdir(dir)) != NULL) {
-            if (ent->d_type == DT_CHR) {
-                char absolute_path[PATH_MAX] = {0};
-                char name[80] = {0};
-
-                strcpy(absolute_path, "/dev/input/");
-                strcat(absolute_path, ent->d_name);
-
-                fd = open(absolute_path, O_RDWR);
-                if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
-                    if (strcmp(name, "fts_ts") == 0 ||
-                            strcmp(name, "NVTCapacitiveTouchScreen") == 0)
-                        break;
-                }
-
-                close(fd);
-                fd = -1;
-            }
-        }
-
-        closedir(dir);
-    }
-
-    return fd;
-}
-
 Return<void> Power::setFeature(Feature /*feature*/, bool /*activate*/) {
-    switch (feature) {
-        case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE: {
-            int fd = open_ts_input();
-            if (fd == -1) {
-                ALOGW("DT2W won't work because no supported touchscreen input devices were found");
-                return Void();
-            }
-            struct input_event ev;
-            ev.type = EV_SYN;
-            ev.code = SYN_CONFIG;
-            ev.value = activate ? kWakeupModeOn : kWakeupModeOff;
-            write(fd, &ev, sizeof(ev));
-            close(fd);
-            } break;
-        default:
-            break;
-    }
+    // Nothing to do
     return Void();
 }
 
